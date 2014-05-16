2cfe83853b368834397836a7eda280050b7fc096
Franz Heidl <franz@franzheidl.de>
2014-05-17 00:18:22 +0200

    Release v1.0.2

--- lib/spinners.rb
+++ lib/spinners.rb
@@ -3,6 +3,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('spinners', :path => extension_path)
 
 module ModernizrMixin
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
   DATE = "2014-05-16"
 end
