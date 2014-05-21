2ed546a8cf7e14831d99020c93216389449fb85d
Franz Heidl <franz@franzheidl.de>
2014-05-21 19:12:14 +0200

    fix module name

--- lib/spinners.rb
+++ lib/spinners.rb
@@ -2,7 +2,7 @@ require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('spinners', :path => extension_path)
 
-module ModernizrMixin
+module spinners
   VERSION = "1.0.2"
   DATE = "2014-05-16"
 end
